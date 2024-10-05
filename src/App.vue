cd6c409014d1e7f2ba3a75262aa3c3dc714cc753
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-10-05 09:31:12 +0300

    Implement html layout

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,201 @@
-<template>Hello, Vue!</template>
+<template>
+  <div class="min-h-screen bg-neutral-100 py-8">
+    <div class="container mx-auto flex flex-col gap-4 items-center py-8">
+      <section
+        class="w-full bg-white border-solid border border-neutral-200 rounded p-4 flex gap-4"
+      >
+        <div
+          class="flex-1 border border-neutral-200 rounded py-1 px-2 flex gap-2 items-center"
+        >
+          <button
+            class="bg-sky-500 hover:bg-red-400 ease-out duration-300 rounded h-5 flex gap-2 justify-between items-center px-1"
+          >
+            <p class="text-xs font-bold text-white">
+              Amsterdam
+            </p>
+            <span class="text-xs text-white font-bold">X</span>
+          </button>
+          <button
+            class="bg-sky-500 hover:bg-red-400 ease-out duration-300 rounded h-5 flex gap-2 justify-between items-center px-1"
+          >
+            <p class="text-xs font-bold text-white">
+              Washington
+            </p>
+            <span class="text-xs text-white font-bold">X</span>
+          </button>
+          <p class="text-neutral-400">
+            Winners
+          </p>
+        </div>
+        <button
+          class="bg-sky-400 hover:bg-sky-500 ease-out duration-300 rounded px-3 py-1 font-bold text-white"
+        >
+          New winner
+        </button>
+      </section>
+      <section
+        class="flex flex-col gap-4 w-full bg-white border-solid border border-neutral-200 rounded p-4"
+      >
+        <hgroup>
+          <h2 class="text-neutral-900 font-bold uppercase">
+            Register form
+          </h2>
+          <p class="text-neutral-400">
+            Please fill all the fields
+          </p>
+        </hgroup>
+        <form class="flex flex-col gap-3">
+          <div class="flex flex-col">
+            <label
+              for="name"
+              class="font-bold text-neutral-900 mb-1"
+            >Name</label>
+            <input
+              type="text"
+              placeholder="Enter user name"
+              class="border-2 border-red-400 rounded py-1 px-3 bg-neutral-100"
+            >
+            <div class="text-red-500 text-sm">
+              This value is required
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <label
+              for="birth-date"
+              class="font-bold text-neutral-900 mb-1"
+            >Date of Birth</label>
+            <input
+              id="birth-date"
+              type="date"
+              name="birth-date"
+              placeholder="Enter user name"
+              class="border border-neutral-200 rounded py-1 px-3 bg-neutral-100"
+            >
+            <div class="text-red-500 text-sm hidden">
+              This value is required
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <label
+              for="email"
+              class="font-bold text-neutral-900 mb-1"
+            >Email</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              class="border border-neutral-200 rounded py-1 px-3 bg-neutral-100"
+            >
+            <div class="text-red-500 text-sm hidden">
+              This value is required
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <label
+              for="phone"
+              class="font-bold text-neutral-900 mb-1"
+            >Phone number</label>
+            <input
+              id="phone"
+              type="tel"
+              name="phone"
+              placeholder="Enter Phone number"
+              class="border border-neutral-200 rounded py-1 px-3 bg-neutral-100"
+            >
+            <div class="text-red-500 text-sm hidden">
+              This value is required
+            </div>
+          </div>
+          <div class="flex flex-row justify-end mt-6">
+            <button
+              class="bg-sky-400 hover:bg-sky-500 rounded px-5 py-1 font-bold text-white ease-out duration-300"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+      <section
+        class="flex flex-col gap-4 w-full bg-white border-solid border border-neutral-200 rounded p-4"
+      >
+        <table class="table-auto">
+          <thead
+            class="border-b border-neutral-200 font-medium text-neutral-900"
+          >
+            <tr>
+              <th class="px-6 py-4 font-bold text-left">
+                #
+              </th>
+              <th class="px-6 py-4 font-bold text-left">
+                Name
+              </th>
+              <th class="px-6 py-4 font-bold text-left">
+                Date of Birth
+              </th>
+              <th class="px-6 py-4 font-bold text-left">
+                Email
+              </th>
+              <th class="px-6 py-4 font-bold text-left">
+                Phone number
+              </th>
+            </tr>
+          </thead>
+          <tbody class="text-neutral-500">
+            <tr class="border-b border-neutral-200">
+              <td class="px-6 py-4 font-medium">
+                1
+              </td>
+              <td class="px-6 py-4">
+                Amsterdam
+              </td>
+              <td class="px-6 py-4">
+                10/10/1990
+              </td>
+              <td class="px-6 py-4">
+                email@domain.com
+              </td>
+              <td class="px-6 py-4">
+                (380) 123-1234
+              </td>
+            </tr>
+            <tr class="border-b border-neutral-200">
+              <td class="px-6 py-4 font-medium">
+                1
+              </td>
+              <td class="px-6 py-4">
+                Amsterdam
+              </td>
+              <td class="px-6 py-4">
+                10/10/1990
+              </td>
+              <td class="px-6 py-4">
+                email@domain.com
+              </td>
+              <td class="px-6 py-4">
+                (380) 123-1234
+              </td>
+            </tr>
+            <tr class="border-b border-neutral-200">
+              <td class="px-6 py-4 font-medium">
+                1
+              </td>
+              <td class="px-6 py-4">
+                Amsterdam
+              </td>
+              <td class="px-6 py-4">
+                10/10/1990
+              </td>
+              <td class="px-6 py-4">
+                email@domain.com
+              </td>
+              <td class="px-6 py-4">
+                (380) 123-1234
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
+  </div>
+</template>
